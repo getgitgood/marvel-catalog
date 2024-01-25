b33bd4c0020c518ccabbb87c410e8bf8e9dabc2d
Kotec <vivalastelmah@gmail.com>
2024-01-25 20:25:37 +0300

    fix: sticky header and body bg color issues

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,20 +3,24 @@ import { CardData } from '../types';
 import { Link } from 'react-router-dom';
 import getImageSrc from '../utils/getImageSrc';
 
-const StyledCard = styled(Link)`
+const StyledCard = styled.div`
   display: flex;
   justify-content: center;
-  .card_image {
-    padding: 0.5em;
-    box-shadow: 0 2px 5px 4px rgba(0, 0, 0, 0.5);
-    cursor: pointer;
-    user-select: none;
-    border-radius: 5px;
-    max-width: 25em;
 
+  .card_link {
+    padding: 0.5em;
+    max-width: 25em;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px 4px rgba(0, 0, 0, 0.5);
     @media screen and (max-width: ${({ theme }) => theme.mobile}) {
       max-width: 15em;
     }
+  }
+  .card_image {
+    cursor: pointer;
+    user-select: none;
+    width: 100%;
+    height: 100%;
   }
 `;
 
@@ -31,8 +35,10 @@ export default function Card({ cardData }: CardProps) {
   const imageSrc = getImageSrc(images);
 
   return (
-    <StyledCard to={String(id)}>
-      <img className="card_image" src={imageSrc} />
+    <StyledCard>
+      <Link className="card_link" to={String(id)}>
+        <img className="card_image" src={imageSrc} />
+      </Link>
     </StyledCard>
   );
 }
